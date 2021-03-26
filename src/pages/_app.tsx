80e7c54735f9389047c4aa8b211a3eeb072d1a5f
Takuya Kojima <t.kojima.work@gmail.com>
2021-03-26 21:15:10 +0900

    set absolute alias paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import '../styles/normalize.css'
+import '@/styles/normalize.css'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
